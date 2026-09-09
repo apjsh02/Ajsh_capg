b892ae5dc6a5c51a6a96361bd1107e294c667cea
Pushkarchandra Joshi <anjali.pushkarchandra-joshi@capgemini.com>
2026-09-09 16:37:22 +0530

    'v1'

---
+++ cap2.py
@@ -0,0 +1,84 @@
+# a=['apple','grapes','oranges','guava']
+# c=['apples','graps','orangs','guav']
+# d=a+c
+# print(type(a))
+# b=a[1:3]
+# c[3]='hj'
+# d[3:5]=['1k','gg']
+# print(d)
+# a=['car','cycle','bike','truck']
+# c=['plane','bus','train','auto']
+# p=a+c
+# print(type(a))
+# print(type(c))
+# print(type(p))
+# b=a[1:3]
+# print(b)
+# thislist = [a]
+# a.append("orange")
+# print(a)
+# thistuple = ("apple", "banana", "cherry")
+# print(type(thistuple))
+# x = ("apple", "banana", "cherry")
+# y = list(x)
+# y[1] = "kiwi"
+# x = tuple(y)
+
+# print(x)
+# thistuple = ("apple", "banana", "cherry")
+# y = list(thistuple)
+# y.append("orange")
+# thistuple = tuple(y)
+# print(thistuple)
+# thistuple = ("apple", "banana", "cherry")
+# y = list(thistuple)
+# y.remove("apple")
+# thistuple = tuple(y)
+# print(thistuple)
+# fruits = ("apple", "banana", "cherry")
+# (green, yellow, red) = fruits
+# print(green)
+# print(yellow)
+# print(red)
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# print(thisdict)
+# car = {
+# "brand": "Ford",
+# "model": "Mustang",
+# "year": 1964
+# }
+
+# x = car.keys()
+
+# print(x) #before the change
+
+# car["color"] = "white"
+
+# print(x) #after the change
+# car = {
+# "brand": "Ford",
+# "model": "Mustang",
+# "year": 1964
+# }
+
+# x = car.values()
+
+# print(x) #before the change
+
+# car["color"] = "white"
+
+# print(x) #after the change
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# if "model" in thisdict:
+#   print("Yes, 'model' is one of the keys in the thisdict dictionary")
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
---
+++ index.py
@@ -0,0 +1,71 @@
+print('Hello everyone')
+#this is a sample code
+print('''Hello everyone' \
+'ttgyr''')
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+Vs='Anjali'
+bs=' Joshi'
+print(Vs+''+bs)
+x = 5
+y = 10
+print(x + y)
+x = 'Anjali Joshi'
+y = 20
+print('name:',x,'id:',y)
+a=150
+b=1400
+if a>b:
+    print('a is less than b')
+elif b>a:
+    print('b')   
+else:
+    print('yeah')
+
+a=19
+if a>18:
+    print('eligible')
+else:
+    ('not eligible')
+
+a = 5
+y = 10
+print(a * y)
+x= 400
+y= 40
+print(x + y)
+a=16
+if (a>18):
+    print('voter id')
+else:
+    print('no voter id')
+x=51
+if(85<x<100):
+    print('Grade A')
+elif(65<x<85):
+    print('Grade B')
+else:
+    print('fail')
+
+b=int(input('enter your first number :'))
+c=int(input('enter your second number :'))
+p=b+c
+print(p)
+
+b=int(input('enter your age :'))
+if b>18:
+    print('eligible')
+else:
+    ('not eligible')
+b=int(input('enter your first number :'))
+c=int(input('enter your second number :'))
+p=b*c
+print(p)
+x=int(input('enter your first number :'))
+if(85<x<100):
+    print('Grade A')
+elif(65<x<85):
+    print('Grade B')
+else:
+    print('fail')
