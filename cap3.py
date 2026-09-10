afb6eabec5cfc7b99d1396e900e80a338f43a7e0
Pushkarchandra Joshi <anjali.pushkarchandra-joshi@capgemini.com>
2026-09-10 17:38:52 +0530

    'v1'

---
+++ cap3.py
@@ -0,0 +1,45 @@
+# def my_function():
+#   print("Hello from a function")
+# my_function()
+# def my_function(fname):
+#   print(fname + " Refsnes")
+
+# my_function("Emil")
+# my_function("Tobias")
+# my_function("Linus")
+# def my_function(fname, lname):
+#   print(fname + " " + lname)
+
+# my_function("Anjali", "Joshi")
+# def my_function(*kids):
+#   print("The youngest child is " + kids[0])
+
+# my_function("Emil", "Tobias", "Linus")
+# def countdown(n):
+#   if n <= 0:
+#     print("Done!")
+#   else:
+#     print(n)
+#     countdown(n - 1)
+
+# countdown(5)
+# def add(a, b):
+#     return a + b
+# print("Addition:", add(10, 20))
+# def check_even_odd(num):
+#     return "Even" if num % 2 == 0 else "Odd"
+# print("Even/Odd:", check_even_odd(18))
+# height = float(input('enter your height in cm'))
+# weight = float(input('enter your weight in kg'))
+# #convert cm to meters
+# height= height/ 100
+# bmi = weight/ (height*height)
+# print('your bmi is:', round(bmi,2))
+# if bmi < 18.5:
+#     print('underweight')
+# elif bmi < 25:
+#     print('normal')
+# elif bmi <30:
+#     print('overweight')
+# else:
+#     print('obsese')
---
+++ task3.py
@@ -0,0 +1,30 @@
+# def add(a, b):
+# #     return a + b
+# # print("Addition:", add(10, 20))
+# def check_even_odd(num):
+#     return "Even" if num % 2 == 0 else "Odd"
+# print("Even/Odd:", check_even_odd(18))
+# def find_max(a, b, c):
+#     return max(a, b, c)
+# print("Maximum:", find_max(25, 40, 18))
+# def factorial(n):
+#     if n < 0:
+#         return "Factorial not defined for negative numbers"
+#     result = 1
+#     for i in range(1, n + 1):
+#         result *= i
+#         return result
+# print("Factorial:", factorial(5))
+ 
+# def count_vowels(text):
+#     vowels = "aeiouAEIOU"
+#     count = 0
+#     for char in text:
+#         if char in vowels:
+#             count += 1
+#             return count
+# print("Vowels Count:", count_vowels("Hello World"))
+# def circle_area(radius):
+#     pi = 3.14
+#     return pi * radius * radius
+# print("Circle Area:", circle_area(7))
